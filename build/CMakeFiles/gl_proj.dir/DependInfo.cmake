00f7a9c96db7d48ca155e320c50aeb1cc570468d
Trent Moyar <trentmoyar@gmail.com>
2022-02-21 01:58:08 -0500

    Initial

---
+++ build/CMakeFiles/gl_proj.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/trent/Projects/gl_proj/glad.c" "CMakeFiles/gl_proj.dir/glad.c.o" "gcc" "CMakeFiles/gl_proj.dir/glad.c.o.d"
+  "/home/trent/Projects/gl_proj/main.cpp" "CMakeFiles/gl_proj.dir/main.cpp.o" "gcc" "CMakeFiles/gl_proj.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/trent/Projects/gl_proj/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
